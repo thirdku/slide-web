908e04587ae915b2bb7cad6eb26939ff37a2d8d1
EC2 Default User <ec2-user@ip-172-31-44-21.ec2.internal>
2020-09-29 15:20:39 +0000

    add

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Row, Col} from 'antd';
+import 'antd/dist/antd.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className="App-back">
+     <Row className="App-header1" gutter={16,16} >
+     <Col span={6}><div className="Button">Back</div></Col>
+     <Col span={14}> </Col>
+     <Col span={2} ><div className="Button-big"></div></Col>
+     <Col span={2} ><div className="Button-big"></div></Col>
+     </Row>
+        <Row className="App-header" gutter={16} >
+      <Col span={6} ><div className="Column"><div className="Flexbox">col-12</div><div className="Flexbox">col-12</div><div className="Flexbox">col-12</div><div className="Flexbox">col-12</div><div className="Flexbox">col-12</div><div className="Flexbox">col-12</div></div></Col>
+      <Col span={18} className="Column-big"><div>col-12</div></Col>
+    </Row>
+    </div>
     </div>
   );
 }
