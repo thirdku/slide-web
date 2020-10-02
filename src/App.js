e4c694ce15f3ad166936ccce11affae43548187e
EC2 Default User <ec2-user@ip-172-31-32-129.ec2.internal>
2020-10-02 09:38:02 +0000

    dragdrop

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React from 'react';
-import { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState,useEffect } from 'react';
 import { Row, Col, Button} from 'antd';
 import 'antd/dist/antd.css';
 import Slides from './Slides.js';
 import SlideView from './SlideView.js';
 import { BellOutlined, UserOutlined } from '@ant-design/icons';
+import { DragDropContext } from "react-beautiful-dnd";
 
 
 export const styles = {
  app:{
   textAlign:'center'
-},
-appHeader: {
+ },
+ appHeader: {
   backgroundColor: '#282c34',
   minHeight: '84.8vh',
   minWidth: '100%',
@@ -24,9 +23,8 @@ appHeader: {
   paddingRight: '80px',
   paddingTop:  '0px',
   paddingBottom: '40px',
-
-},
-appHeader1: {
+ },
+ appHeader1: {
   backgroundColor: '#282c34',
   minHeight: '10px',
   minWidth: '100%',
@@ -40,80 +38,78 @@ appHeader1: {
   paddingTop:  '40px',
   paddingBottom: '40px',
   marginRight: '-8px',
-
-},
-column: {
-border: '1px solid #73AD21',
-minHeight: '84.8vh',
-  
-},
-columnBig: {
-border: '1px solid #73AD21',
-paddingLeft: '10px',
-backgroundColor: 'white',
-  
-},
-button: {
+ },
+ column: {
+  border: '1px solid #73AD21',
+  minHeight: '84.8vh',
+ },
+ columnBig: {
+  border: '1px solid #73AD21',
+  paddingLeft: '10px',
+  backgroundColor: 'white',
+ },
+ button: {
   backgroundColor: 'black',
   height: '40px',
   textAlign: 'center',
   fontSize: '17px',
   padding: '8px',
-
-},
-back: {
+ },
+ back: {
   width:'100px',
   height:'60px',
   backgroundColor: '#282c34',
-
-},
-buttonBig: {
+ },
+ buttonBig: {
   backgroundColor: 'black',
   height: '60px',
   textAlign: 'center',
   fontSize: '17px',
   padding: '12px',
-
-},
-flexbox: {
- height : '100px',
- margin : '15px',
- marginLeft: '30px',
- marginRight: '30px',
- textAlign: 'center',
- border: '1px solid #73AD21',
- backgroundColor: 'white',
- color: 'black',
- position: 'relative'
-},
-title:{
+ },
+ flexbox: {
+  height : '100px',
+  margin : '15px',
+  marginLeft: '30px',
+  marginRight: '30px',
+  textAlign: 'center',
+  border: '1px solid #73AD21',
+  backgroundColor: 'white',
+  color: 'black',
+  position: 'relative'
+ },
+ title:{
   fontSize: '25px',
-},
-data:{
- fontSize: '10px',
- marginTop:'-13px',
-},
-smallButton:{
- position: 'absolute',
- top: '0px',
- right: '0px'
-},
-smallButton1:{
-    position:'absolute',
-    right: '23px'
-}
+ },
+ data:{
+  fontSize: '10px',
+  marginTop:'-13px',
+ },
+ smallButton:{
+  position: 'absolute',
+  top: '0px',
+  right: '0px'
+ },
+ smallButton1:{
+  position:'absolute',
+  right: '23px'
+ }
+ };
 
-}
 function App() {
-    
+ 
   return (
     <div style={styles.app}>
      <div >
-      <Row style={styles.appHeader1} gutter={16} >
+      <Row 
+       style={styles.appHeader1} 
+       gutter={16} 
+      >
        <Col span={6}>
-        <Button block>Back</Button>
+        <Button   block >Back</Button>
        </Col>
-       <Col span={14}> </Col>
+       <Col span={14}> 
+       </Col>
        <Col span={2} >
         <Button  icon={<BellOutlined />}>
         </Button>
@@ -123,13 +119,16 @@ function App() {
         </Button>
        </Col>
       </Row>
-      <Row style={styles.appHeader} gutter={16,16} >
-        <Slides />
+      <Row 
+       style={styles.appHeader} 
+       gutter={16,16} 
+      >
+        <Slides   />
         <SlideView/>
       </Row>
      </div>
     </div>
   );
-}
+ }
 
 export default App;
--- src/Slides.js
+++ src/Slides.js
@@ -1,33 +1,98 @@
 import React from 'react';
-import logo from './logo.svg';
 import { useState } from 'react';
-import './App.css';
-import { Row, Col, Button} from 'antd';
+import { Droppable , DragDropContext, Draggable } from "react-beautiful-dnd";
+import { PlusOutlined, MinusOutlined } from '@ant-design/icons';
+
+import { Col , Button} from 'antd';
 import 'antd/dist/antd.css';
 import { styles } from './App.js';
-import { PlusOutlined, MinusOutlined } from '@ant-design/icons';
 import SlidesItems from './SlidesItems.js';
-const datas = [{title:"fake",data:"Nimble Strikes: Your critical strikes with Attacks reduce your non-Ultimate ability cooldowns by 20% of their remaining cooldown"},{title:"dod",data:"Every third Attack is enhanced, dealing an additional 80-120 (lv 1-18) (+30% bonus Attack Damage) true damage. "},{title:"ga",data:"When taking damage that would reduce you below 30% health, gain a 150-650 (lv 1-18) shield for 3 seconds. In addition, gain 20% Life Steal for 8 seconds. (90 second cooldown) "},{title:"faker",data:"Dash in target direction, firing three missiles at the lowest-health enemy near your destination (prioritizing champions). Deals a total of 105-300 (lv 1-18) (+30% "},{title:"aagaa",data:" Deal 100 (+30% Ability Power) damage in a cone, slowing enemies by 65% for 1.5 seconds. Enemies at the center of the cone are Rooted instead. (45 second cooldown) "}]
- 
+
+const datas = [{title:"fake",data:"Nimble Strikes: Your critical strikes with Attacks reduce your non-Ultimate ability cooldowns by 20% of their remaining cooldown",id:"id-1"},
+ {title:"dod",data:"Every third Attack is enhanced, dealing an additional 80-120 (lv 1-18) (+30% bonus Attack Damage) true damage. ",id:"id-2"},
+ {title:"ga",data:"When taking damage that would reduce you below 30% health, gain a 150-650 (lv 1-18) shield for 3 seconds. In addition, gain 20% Life Steal for 8 seconds. (90 second cooldown)",id:"id-3"},
+ {title:"faker",data:"Dash in target direction, firing three missiles at the lowest-health enemy near your destination (prioritizing champions). Deals a total of 105-300 (lv 1-18) (+30% ",id:"id-4"},
+ {title:"aagaa",data:" Deal 100 (+30% Ability Power) damage in a cone, slowing enemies by 65% for 1.5 seconds. Enemies at the center of the cone are Rooted instead. (45 second cooldown) ",id:"id-5"}
+ ]
+
 export default function Slides() {
     const [data, setData] = useState(datas);
-    
+    const onDragEnd = result => {
+      // return if item was dropped outside
+      if (!result.destination) return;
+      // return if the item was dropped in the same place
+      if (
+       result.source.droppableId === result.destination.droppableId &&
+       result.source.index === result.destination.index
+       )
+       return;
+      // get the items array
+      const newItems = [...data];
+      // get the draggedItems
+      const draggedItem = newItems[result.source.index];
+      // delete the item from source position and insert it to the destination positon
+      newItems.splice(result.source.index, 1);
+      newItems.splice(result.destination.index, 0, draggedItem);
+      // create new data
+      const newData = [
+       ...newItems
+      ];
+      // update state
+      setData(newData);
+      console.log(newItems);
+    };
     const onDelete = (index) => {
      const item = [...data];
-     item.splice(index,1)
-     setData(item)
-    }
+     item.splice(index,1);
+     setData(item);
+    };
   return (
-        
-       
+       <DragDropContext onDragEnd={onDragEnd}>
         <Col span={6} >
-         <div style={styles.column}>
+        <Droppable droppableId={"droppable"}>
+          {provided => (
+         <div 
+          style={styles.column} 
+          {...provided.droppableProps}
+          ref={provided.innerRef}
+         >
          { data.map ((item,index) => (
-             <SlidesItems key={item.id} index={index} item={item} onDelete={onDelete}/>
+           <Draggable 
+            key={item.id}
+            draggableId={item.id} 
+            index={index}>
+            {provided => (
+             <div 
+              ref={provided.innerRef}
+              {...provided.draggableProps}
+              {...provided.dragHandleProps}
+              >
+              <div style={styles.flexbox}>
+               <Button 
+                style={styles.smallButton} 
+                icon={<MinusOutlined />} 
+                size="small" 
+                onClick={()=>onDelete(index)} 
+                type="ghost"
+               >
+               </Button>
+               <h6 style={styles.title}>
+                {item.title}
+               </h6> 
+               <p style={styles.data}>
+               {item.data} 
+               </p>
+              </div>
+             </div>
+             )}
+            </Draggable>
          )
-         )}
-         </div>
-        </Col>
-     
-        
         )}
+          {provided.placeholder}
+         </div>
+         )}
+        </Droppable>
+        </Col>
+       </DragDropContext>
+        
+    )}
